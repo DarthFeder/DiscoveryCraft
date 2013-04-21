9dd7436337da3de7a308ae328d1f4dbc620e90b7
Darth_Feder <darth.feder.hi@gmail.com>
2013-04-21 19:25:59 -0400

    Skeleton of mod: mcmod.info, packages, reference class, and main class. Will start on features soon.

---
+++ dc_common/mods/minecraft/darth/dc/DiscoveryCraft.java
@@ -0,0 +1,50 @@
+package mods.minecraft.darth.dc;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import mods.minecraft.darth.dc.lib.Reference;
+
+
+/*
+ * DiscoveryCraft
+ * 
+ * @author Darth_Feder
+ * 
+ * 
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES)
+public class DiscoveryCraft
+{
+
+        @PreInit
+        public void preInit(FMLPreInitializationEvent event)
+        {
+            
+            
+        }
+    
+    
+        @Init
+        public void init(FMLInitializationEvent event)
+        {
+            
+            
+        }
+        
+        
+        @PostInit
+        public void postInit(FMLPostInitializationEvent event)
+        {
+            
+            
+        }
+    
+}
---
+++ dc_common/mods/minecraft/darth/dc/lib/Reference.java
@@ -0,0 +1,13 @@
+package mods.minecraft.darth.dc.lib;
+
+public class Reference
+{
+
+    // General constants
+    public static final String MOD_ID = "DC";
+    public static final String MOD_NAME = "DiscoveryCraft";
+    public static final String VERSION = "pre1";
+    public static final String DEPENDENCIES = "required-after:Forge@[7.7.1.655,)";
+
+    
+}
