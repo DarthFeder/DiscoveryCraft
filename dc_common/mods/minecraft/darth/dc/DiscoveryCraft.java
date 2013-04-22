d1f879d9ae5164b72ed31932c4bad9fd9b544f23
Darth_Feder <darth.feder.hi@gmail.com>
2013-04-22 19:26:36 -0400

    A bit more in terms of skeleton. Maybe I should add another skeleton to the game.

--- dc_common/mods/minecraft/darth/dc/DiscoveryCraft.java
+++ dc_common/mods/minecraft/darth/dc/DiscoveryCraft.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
 import mods.minecraft.darth.dc.lib.Reference;
 
@@ -21,6 +22,7 @@ import mods.minecraft.darth.dc.lib.Reference;
  */
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES, acceptedMinecraftVersions = Reference.MC_VERSIONS, modExclusionList = Reference.EXCLUSION_MODS)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class DiscoveryCraft
 {
 
