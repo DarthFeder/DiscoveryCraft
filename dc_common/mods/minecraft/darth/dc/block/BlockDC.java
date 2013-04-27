124678baae2a7dc2468b8bb7cf82a9b63192f91a
Darth_Feder <darth.feder.hi@gmail.com>
2013-04-27 10:14:25 -0400

    End of this session. Copied in some of Pahimar's code for tile entities and blocks, and did some research on blocks and texturing. Put in iron or as placeholder of unknown ore texture. Creative tab still crashes game.

--- dc_common/mods/minecraft/darth/dc/block/BlockDC.java
+++ dc_common/mods/minecraft/darth/dc/block/BlockDC.java
@@ -1,7 +1,21 @@
 package mods.minecraft.darth.dc.block;
 
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+import mods.minecraft.darth.dc.lib.Reference;
+import mods.minecraft.darth.dc.tileentity.TileDC;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+
 
 public class BlockDC extends Block
 {
@@ -11,6 +25,54 @@ public class BlockDC extends Block
         super(id, material);
     }
     
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName2());
+    }
+
+
     
+    //Sets the direction of the block when placed
+    @Override
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving entityLiving, ItemStack itemStack)
+    {
+        int direction = 0;
+        int facing = MathHelper.floor_double(entityLiving.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+
+
+        switch (facing)
+        {
+            case 0:
+                direction = ForgeDirection.NORTH.ordinal();
+                break;
+                
+            case 1:
+                direction = ForgeDirection.EAST.ordinal();
+                break;
+                
+            case 2:
+                direction = ForgeDirection.SOUTH.ordinal();
+                break;
+                
+            case 3:
+                direction = ForgeDirection.WEST.ordinal();
+                break;
+                
+        };
+
+
+        world.setBlockMetadataWithNotify(x, y, z, direction, 3);
+
+
+        if (itemStack.hasDisplayName())
+        {
+            ((TileDC) world.getBlockTileEntity(x, y, z)).setCustomName(itemStack.getDisplayName());
+        }
+
+        ((TileDC) world.getBlockTileEntity(x, y, z)).setOrientation(direction);
+    }
+
 
 }
---
+++ dc_common/mods/minecraft/darth/dc/tileentity/TileDC.java
@@ -0,0 +1,71 @@
+package mods.minecraft.darth.dc.tileentity;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
+
+public class TileDC extends TileEntity
+{
+
+    private ForgeDirection orientation;
+    private byte state;
+    private String customName;
+    
+    public TileDC()
+    {
+        orientation = ForgeDirection.SOUTH;
+        state = 0;
+        customName = "";
+        
+    }
+    
+    public ForgeDirection getOrientation()
+    {
+        return orientation;
+    }
+    
+    public void setOrientation(ForgeDirection orientation)
+    {
+        this.orientation = orientation;
+    }
+    
+    public void setOrientation(int orientation)
+    {
+        this.orientation = ForgeDirection.getOrientation(orientation);
+    }
+
+    public short getState()
+    {
+        return state;
+    }
+    
+    public void setState(byte state)
+    {
+        this.state = state;
+    }
+
+
+    public boolean hasCustomName()
+    {
+        return customName != null && customName.length() > 0;
+    }
+
+
+    public String getCustomName()
+    {
+        return customName;
+    }
+
+
+    public void setCustomName(String customName)
+    {
+        this.customName = customName;
+    }
+
+
+    public boolean isUseableByPlayer(EntityPlayer player)
+    {
+        return true;
+    }
+
+}
