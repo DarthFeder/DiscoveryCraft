9c6eddbbe472b85a1d81771b9aa3e50ea84123ea
Darth_Feder <darth.feder.hi@gmail.com>
2013-05-25 19:14:11 -0400

    Lot's more screwdriver funcionality. Added in transmuatation and rotation in several things. Yes, might be op, but will be hard to obtain, and will take longer in the future. Also might change transumting to just creative mode. Known bugs: anvils don't change, a powered piston that is rotated gets buggy, and lights do not stay in wrong position.

--- dc_common/mods/minecraft/darth/dc/item/ItemSonicScrewdriver.java
+++ dc_common/mods/minecraft/darth/dc/item/ItemSonicScrewdriver.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -46,17 +47,16 @@ public class ItemSonicScrewdriver extends ItemDC
         
         if (par3World.getBlockId(par4, par5,par6) == Block.tnt.blockID)
         {
-            //playScrewdriverSound(par2EntityPlayer, par3World);
+            
+            //Light tnt
             
         }
         else if (par3World.getBlockId(par4, par5,par6) == Block.redstoneLampIdle.blockID)
         {
-            //playScrewdriverSound(par2EntityPlayer, par3World);
             par3World.setBlock(par4, par5, par6, Block.redstoneLampActive.blockID);
         }
         else if (par3World.getBlockId(par4, par5,par6) == Block.redstoneLampActive.blockID)
         {
-            //playScrewdriverSound(par2EntityPlayer, par3World);
             par3World.setBlock(par4, par5, par6, Block.redstoneLampIdle.blockID);
         }
         else if (par3World.getBlockMaterial(par4, par5, par6) == Material.wood)
@@ -65,6 +65,88 @@ public class ItemSonicScrewdriver extends ItemDC
             par3World.playSoundEffect((double) par4 + 0.5D, (double) par5 + 0.5D, (double) par6 + 0.5D, "random.fizz", 1.0F, par3World.rand.nextFloat() * 0.1F + 0.9F);
             return true;
         }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.sand.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.glass.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.glass.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.sand.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.anvil.blockID && par3World.getBlockMetadata(par4, par5, par6) == 2)
+        {
+            par3World.setBlockMetadataWithNotify(par4, par5, par6, 1, 2);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.anvil.blockID && par3World.getBlockMetadata(par4, par5, par6) == 1)
+        {
+            par3World.setBlockMetadataWithNotify(par4, par5, par6, 0, 2);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.dirt.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.grass.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.grass.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.dirt.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.stone.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.cobblestone.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.cobblestone.blockID)
+        {
+            par3World.setBlock(par4, par5, par6, Block.stone.blockID);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.pumpkin.blockID)
+        {
+            if (par2EntityPlayer.isSneaking())
+            {
+                if (par3World.getBlockMetadata(par4, par5, par6) != 3)
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, par3World.getBlockMetadata(par4, par5, par6) + 1, 2);
+                else
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, 0, 2);
+            }
+            else
+                par3World.setBlock(par4, par5, par6, Block.pumpkinLantern.blockID, par3World.getBlockMetadata(par4, par5, par6), 2);
+        }
+            
+        else if (par3World.getBlockId(par4, par5,par6) == Block.pumpkinLantern.blockID)
+        {
+            if (par2EntityPlayer.isSneaking())
+            {
+                if (par3World.getBlockMetadata(par4, par5, par6) != 3)
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, par3World.getBlockMetadata(par4, par5, par6) + 1, 2);
+                else
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, 0, 2);
+            }
+            else
+                par3World.setBlock(par4, par5, par6, Block.pumpkin.blockID, par3World.getBlockMetadata(par4, par5, par6), 2);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.pistonBase.blockID)
+        {
+            if (par2EntityPlayer.isSneaking())
+            {
+                if (par3World.getBlockMetadata(par4, par5, par6) != 5)
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, par3World.getBlockMetadata(par4, par5, par6) + 1, 2);
+                else
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, 0, 2);
+            }
+            else
+                par3World.setBlock(par4, par5, par6, Block.pistonStickyBase.blockID, par3World.getBlockMetadata(par4, par5, par6), 2);
+        }
+        else if (par3World.getBlockId(par4, par5,par6) == Block.pistonStickyBase.blockID)
+        {
+            if (par2EntityPlayer.isSneaking())
+            {
+                if (par3World.getBlockMetadata(par4, par5, par6) != 5)
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, par3World.getBlockMetadata(par4, par5, par6) + 1, 2);
+                else
+                    par3World.setBlockMetadataWithNotify(par4, par5, par6, 0, 2);
+            }
+            else
+                par3World.setBlock(par4, par5, par6, Block.pistonBase.blockID, par3World.getBlockMetadata(par4, par5, par6), 2);
+        }
+        
         
         return false;
         
