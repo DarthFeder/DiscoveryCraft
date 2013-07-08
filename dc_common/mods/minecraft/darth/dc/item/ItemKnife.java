5cbedb7f192d2a73a85c17b7748695115c6c9a51
Rock48 <cquiglz@gmail.com>
2013-07-08 00:29:10 -0400

    knife works

--- dc_common/mods/minecraft/darth/dc/item/ItemKnife.java
+++ dc_common/mods/minecraft/darth/dc/item/ItemKnife.java
@@ -1,7 +1,11 @@
 package mods.minecraft.darth.dc.item;
 
 import mods.minecraft.darth.dc.DiscoveryCraft;
+import mods.minecraft.darth.dc.entity.projectile.EntityKnifeProjectile;
 import mods.minecraft.darth.dc.lib.Strings;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemKnife extends ItemDC
 {
@@ -10,7 +14,33 @@ public class ItemKnife extends ItemDC
     {
         super(id);
         this.setCreativeTab(DiscoveryCraft.tabDC)
-            .setUnlocalizedName(Strings.KNIFE_NAME);
+            .setUnlocalizedName(Strings.KNIFE_NAME)
+            .setMaxStackSize(16);
     }
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
 
+        boolean flag = par3EntityPlayer.capabilities.isCreativeMode;
+
+        if (flag || par3EntityPlayer.inventory.hasItem(ModItems.knife.itemID))
+        {
+
+            EntityKnifeProjectile entityknife = new EntityKnifeProjectile(par2World, par3EntityPlayer, 1.0F);
+
+            --par1ItemStack.stackSize;
+            par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 1.0F, 1.0F);
+            
+            
+            
+            entityknife.canBePickedUp = 1;
+
+            entityknife.setDamage(0.1D);
+
+            if (!par2World.isRemote)
+            {
+                par2World.spawnEntityInWorld(entityknife);
+            }
+        }
+        return par1ItemStack;
+    }
 }
